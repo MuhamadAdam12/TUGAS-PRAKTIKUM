71475206f34a321b82653b04655180a23086af06
MuhamadAdam12 <114368922+MuhamadAdam12@users.noreply.github.com>
2022-10-25 19:03:02 +0700

    Add files via upload

---
+++ A.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package percobaan6;
+
+/**
+ *
+ * @author ACER
+ */
+public class A {
+    String var_a = "Variabel A";
+    String var_b = "Variabel B";
+    String var_c = "Variabel C";
+    String var_d = "Variabel D";
+    
+    A(){
+        System.out.println("Konstruktor A dijalankan");
+ }
+}
+    
