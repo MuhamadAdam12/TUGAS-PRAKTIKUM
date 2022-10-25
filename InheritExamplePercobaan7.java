03cb907c9d0bb22db8d3baadcc2be1f3e751a9b3
MuhamadAdam12 <114368922+MuhamadAdam12@users.noreply.github.com>
2022-10-25 18:09:00 +0700

    Add files via upload

---
+++ InheritExamplePercobaan7.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package percobaan7;
+
+/**
+ *
+ * @author ACER
+ */
+class Bapak {
+    int a =1;
+    int b=1;
+    public void show_variabel(){
+        System.out.println("NIlai a="+ a);
+        System.out.println("NIlai b="+ b);
+ }
+}
+ class Anak extends Bapak{
+    int c;
+    public void show_Variabel(){
+        System.out.println("NIlai a="+ super.a);
+        System.out.println("NIlai b="+ super.b);
+        System.out.println("NIlai c="+ c);
+ }
+}
+public class InheritExamplePercobaan7 {
+    public static void main(String[] args) {
+        Bapak objectBapak = new Bapak();
+        Anak objectAnak = new Anak();
+        
+        System.out.println("Object Bapak (Superclass):");
+        objectBapak.show_variabel();
+        
+        objectAnak.c=5;
+        System.out.println("Object Anak (Superclass dari Bapak):");
+        objectAnak.show_Variabel();
+ }
+}
+
+
+
+
+   
+
