f10490619021c2ce43d977736bb0c1e7fe7dda7b
MuhamadAdam12 <114368922+MuhamadAdam12@users.noreply.github.com>
2022-10-25 17:55:54 +0700

    Add files via upload

---
+++ Percobaan3.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package percobaan3;
+
+/**
+ *
+ * @author ACER
+ */
+public class Percobaan3 {
+    public class Parent {
+        
+}
+    public class Child extends Parent{
+        int x;
+        public Child() {
+            x = 5;
+        }
+    }
+
+}
+ 
+    
+
