1b4f6a346ce4b45804ddf549ba60c16197493faa
MuhamadAdam12 <114368922+MuhamadAdam12@users.noreply.github.com>
2022-10-25 19:16:51 +0700

    PERCOBAAN 5

---
+++ SadObject.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package percobaan5;
+
+/**
+ *
+ * @author ACER
+ */
+public class SadObject extends MoodyObject{
+    protected String getMood(){
+        return "sad";
+    }
+    public void cry(){
+        System.out.println("Hoo hoo");
+ }
+}
+
+    
+
